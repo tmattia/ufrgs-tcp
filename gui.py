6a36d4123e49df70e2c317871b3dd4b5873096ef
Tomas Mattia <tomas@tomas-desktop.(none)>
2009-11-17 23:55:44 -0200

    Added a text input dialog to QOC inserts

--- gui.py
+++ gui.py
@@ -12,13 +12,15 @@ try:
 except:
     sys.exit(1)
 
+from qoc import Criterion, Option, Question
+
 class GUI:
     def __init__(self):
         '''Application setup'''
-        widgetTree = gtk.glade.XML('gui.glade')
-        widgetTree.signal_autoconnect(self)
+        self.widgetTree = gtk.glade.XML('gui.glade')
+        self.widgetTree.signal_autoconnect(self)
         
-        self.mainWindow = widgetTree.get_widget('window1')
+        self.mainWindow = self.widgetTree.get_widget('window1')
         self.mainWindow.show_all()
     
     def drawingAreaClick(self, widget, event):
@@ -53,19 +55,61 @@ class GUI:
     
     def insertCriterion(self, widget):
         '''Inserts a new criterion on the drawing area.'''
-        print 'criterion'
+        description = self.getTextInput('New criterion', 'Description: ')
+        if description:
+            c = Criterion(description)
+            print c
     
     def insertOption(self, widget):
         '''Inserts a new option on the drawing area.'''
-        print 'option'
+        description = self.getTextInput('New option', 'Description: ')
+        if description:
+            o = Option(description)
+            print o
     
     def insertQuestion(self, widget):
         '''Inserts a new question on the drawing area."'''
-        print 'question'
+        description = self.getTextInput('New question', 'Description: ')
+        if description:
+            q = Question(description)
+            print q
     
     def insertRelationship(self, widget):
         '''Inserts a new relationship on the drawing area.'''
         print 'relationship'
+    
+    def getTextInput(self, title, labelText):
+        '''Shows a dialog window with a text entry. Returns a string with the
+        text entry value.
+        
+        title: window title
+        labelText: text applied to the entry's label
+        '''
+        label = self.widgetTree.get_widget('textInputLabel')
+        label.set_text(labelText)
+        
+        dialog = self.widgetTree.get_widget('textInputDialog')
+        dialog.set_title(title)
+        
+        response = dialog.run()
+        dialog.hide()
+        
+        entry = self.widgetTree.get_widget('textInputEntry')
+        text = ''
+        if response == 1:
+            text = entry.get_text()
+        entry.set_text('')
+        return text
+    
+    def textInputDialogOk(self, widget):
+        '''Handles the dialog's ok button event.'''
+        dialog = self.widgetTree.get_widget('textInputDialog')
+        dialog.response(1)
+    
+    def textInputDialogCancel(self, widget):
+        '''Handles the dialog's cancel button event.'''
+        dialog = self.widgetTree.get_widget('textInputDialog')
+        dialog.response(0)
 
 if __name__ == '__main__':
     '''If the script is run directly through a command line (e.g.
